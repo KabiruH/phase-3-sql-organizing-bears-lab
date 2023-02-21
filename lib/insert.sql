09b66fdbc87a69f282f8b6244df67bcdde464b16
King <kelkab46@gmail.com>
2023-02-21 06:23:06 +0300

    completed assignment

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "Black", "aggresive", 1);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "F", "Grey", "pasive", 1);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "M", "Black", "pasive", 1);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "Brown", "Docile", 1);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "Black", "aggresive", 0);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 9, "F", "White", "pasive", 1);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "M", "Brown", "aggresive", 1);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "White", "aggresive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("red", 7, "M", "Black", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("orange", 5, "F", "Grey", "pasive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("yellow", 6, "M", "Black", "aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("green", 8, "M", "Brown", "pasive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("blue", 4, "F", "Black", "aggresive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("indigo", 9, "F", "White", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("violet", 7, "M", "Brown", "pasive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "White", "docile", 1);
